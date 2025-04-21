d6fc9942225f9ac1f8a3e0a1c6d60c8070b82380
michelledantas <michelledantasluiz@gmail.com>
2025-04-21 16:26:14 -0300

    refactor: refactor method calculateDaysAboveAverage for adding try/catch

--- tdd/src/main/java/br/com/ifsp/TemperatureAnalyzer.java
+++ tdd/src/main/java/br/com/ifsp/TemperatureAnalyzer.java
@@ -3,11 +3,17 @@ package br.com.ifsp;
 import java.util.Arrays;
 
 public class TemperatureAnalyzer {
-    public int calculateDaysAboveAverage(String input) {
-        int[] temperature = Arrays.stream(input.split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
-        double average = Arrays.stream(temperature).average().orElse(0);
-        return (int) Arrays.stream(temperature).filter(temp -> temp > average).count();
+    public Object calculateDaysAboveAverage(String input) throws Exception {
+        try{
+            int[] temperature = Arrays.stream(input.split(" "))
+                    .mapToInt(Integer::parseInt)
+                    .toArray();
+            double average = Arrays.stream(temperature).average().orElse(0);
+            return (int) Arrays.stream(temperature).filter(temp -> temp > average).count();
+
+        } catch (NumberFormatException e){
+            throw new IllegalArgumentException("Invalid input: contains non-numeric values", e);
+        }
+
     }
 }
